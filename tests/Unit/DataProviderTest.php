e9484b97be051655bde50e0be6cf84d58863330c
saeidtaheri <taheri.master@gmail.com>
2023-02-01 00:33:01 +0330

    update unit tests

--- tests/Unit/DataProviderTest.php
+++ tests/Unit/DataProviderTest.php
@@ -2,15 +2,17 @@
 
 namespace tests\Unit;
 
+use App\Providers\Csv;
 use App\Providers\DataProvider;
+use App\Providers\Url;
 use PHPUnit\Framework\TestCase;
 
 class DataProviderTest extends TestCase
 {
     public function test_provide_data_from_csv_provider()
     {
-        $provider = new DataProvider();
-        $data = $provider->fromCsv()->prepare();
+        $provider = new DataProvider(new Csv());
+        $data = $provider->prepare();
 
         $this->assertIsArray($data);
         $this->assertNotEmpty($data);
@@ -19,8 +21,8 @@ class DataProviderTest extends TestCase
 
     public function test_provide_data_from_url_provider()
     {
-        $provider = new DataProvider();
-        $data = $provider->fromUrl()->prepare();
+        $provider = new DataProvider(new Url());
+        $data = $provider->prepare();
 
         $this->assertIsArray($data);
         $this->assertNotEmpty($data);
@@ -29,8 +31,8 @@ class DataProviderTest extends TestCase
 
     public function test_provide_data_from_csv_and_url_provider()
     {
-        $provider = new DataProvider();
-        $data = $provider->fromCsv()->fromUrl()->prepare();
+        $provider = new DataProvider(new Csv(), new Url());
+        $data = $provider->prepare();
 
         $this->assertIsArray($data);
         $this->assertNotEmpty($data);
--- tests/Unit/FileDriverTest.php
+++ tests/Unit/FileDriverTest.php
@@ -9,32 +9,47 @@ class FileDriverTest extends TestCase
 {
     public function test_throw_exception_if_provide_a_wrong_config()
     {
-        $this->expectExceptionMessage('driver config is invalid!');
+        $this->expectExceptionMessage('Driver config is invalid!');
 
         $config = [];
+
         $fileDriver = new File($config);
 
         $fileDriver->save(['test']);
     }
 
-    public function test_can_save_to_file()
+    public function test_throw_exception_if_file_directory_is_not_valid()
     {
+        $this->expectExceptionMessage('File directory is invalid!');
+
         $config = [
-            'connections' => [
-                'file' => [
-                    'file_name' => 'users_test.txt',
-                    'dest_path' => getcwd() . '/storage/'
-                ]
-            ]
+            'file_name' => 'users_test.txt',
+            'dest_path' => './storaged/'
         ];
 
         $fileDriver = new File($config);
-        $file = './storage/' . $config['connections']['file']['file_name'];
-        $fileDriver->save(['test']);
+        $file = './storage/' . $config['file_name'];
 
+        $this->assertSame("Users imported!", $fileDriver->save(['test']));
         $this->assertFileExists($file);
         $this->assertStringNotEqualsFile($file, '');
-        $this->expectOutputString("Users imported!\n");
+
+        unlink($file);
+    }
+
+    public function test_can_save_to_file()
+    {
+        $config = [
+            'file_name' => 'users_test.txt',
+            'dest_path' => './storage/'
+        ];
+
+        $fileDriver = new File($config);
+        $file = './storage/' . $config['file_name'];
+
+        $this->assertSame("Users imported!", $fileDriver->save(['test']));
+        $this->assertFileExists($file);
+        $this->assertStringNotEqualsFile($file, '');
 
         unlink($file);
     }
